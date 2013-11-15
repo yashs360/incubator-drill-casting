0e556c48d1857096f6fcaffb4b4e3e07407f57d7
Yash <yash360@gmail.com>
2013-11-15 13:35:13 +0530

    changed operator resolution by adding getCost method

--- exec/java-exec/src/main/java/org/apache/drill/exec/resolver/OperatorFunctionResolver.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/resolver/OperatorFunctionResolver.java
@@ -18,37 +18,33 @@ public class OperatorFunctionResolver implements FunctionResolver {
 		
 		if(args.size() != 2){
 			// TODO: Some Exception
-		}		
-		
-		MajorType m1 = args.get(0).getMajorType();
-		MajorType m2 = args.get(1).getMajorType();
-		
-		Integer t1 = ResolverTypePrecedence.precedenceMap.get(args.get(0).getMajorType().getMinorType().name()); //BIGINT, REQUIRED
-		Integer t2 = ResolverTypePrecedence.precedenceMap.get(args.get(1).getMajorType().getMinorType().name()); // FLOAT8, REQUIRED
-		
-		if(t1==null || t2==null){
-			// TODO: precedence not defined in MAP
-		}
-		else{
-			MajorType commonType = (t1 > t2) ? args.get(0).getMajorType() : args.get(1).getMajorType();
-		}
-		
-		/** Create New arguments **/
-		//List<LogicalExpression> newargs = Lists.newArrayList();
-	     // for (int i = 0; i < call.args.size(); ++i) {
-	     //   LogicalExpression newExpr = // TODO
-	     //   args.add(newExpr);
-	     // }
-		
-		/** Modify function call **/
-		//FunctionCall modifiedCall = new FunctionCall(call.getDefinition(), newargs, call.getPosition());
+		}			
 		 
+		int bestcost = Integer.MAX_VALUE;
+		int currcost = Integer.MAX_VALUE;
+		DrillFuncHolder bestmatch = null;
 		
 		for(DrillFuncHolder h : methods){
-		      if(h.matches(call)){
-		        return h;
-		      }
-		    }
+			currcost = h.getCost(call);
+			
+			if(currcost == -1){
+				//TODO: check for explicit type cast here
+				continue;
+			}
+			
+			if(currcost < bestcost){
+				bestcost = currcost;
+				bestmatch = h;
+			}	      
+		}
+		
+		/** Convert function call here and return back **/
+		// TODO
+		
+		if(bestmatch.matches(call)){
+	        return bestmatch;
+	    }
+	      
 		
 		return null;
 	}
